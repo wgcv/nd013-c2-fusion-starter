8572dfed5ee64a1e1527577456fbdfd19c00cee1
venkatram-dev <45727389+venkatram-dev@users.noreply.github.com>
2023-01-11 10:19:47 -0800

    step3_1_obj_detect

--- student/objdet_detect.py
+++ student/objdet_detect.py
@@ -25,10 +25,12 @@ sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 # model-related
 from tools.objdet_models.resnet.models import fpn_resnet
 from tools.objdet_models.resnet.utils.evaluation_utils import decode, post_processing 
+from tools.objdet_models.resnet.utils.torch_utils import _sigmoid
 
 from tools.objdet_models.darknet.models.darknet2pytorch import Darknet as darknet
 from tools.objdet_models.darknet.utils.evaluation_utils import post_processing_v2
 
+#from utils.torch_utils import _sigmoid
 
 # load model-related parameters into an edict
 def load_configs_model(model_name='darknet', configs=None):
@@ -61,6 +63,50 @@ def load_configs_model(model_name='darknet', configs=None):
         ####### ID_S3_EX1-3 START #######     
         #######
         print("student task ID_S3_EX1-3")
+        
+        configs.model_path = os.path.join(parent_path, 'tools', 'objdet_models', 'resnet')
+        configs.pretrained_filename = os.path.join(configs.model_path, 'pretrained', 'fpn_resnet_18_epoch_300.pth')
+        configs.arch = 'fpn_resnet_18'
+        configs.batch_size = 4
+        configs.num_workers = 4
+        #configs.cfgfile = os.path.join(configs.model_path, 'config', 'complex_yolov4.cfg')
+        configs.conf_thresh = 0.5
+        #configs.distributed = False
+        #configs.img_size = 608
+        #configs.nms_thresh = 0.4
+        #configs.num_samples = None
+        
+        #configs.pin_memory = True
+        #configs.use_giou_loss = False
+        
+        ##
+        
+        configs.pin_memory = True
+        configs.distributed = False  # For testing on 1 GPU only
+        configs.input_size = (608, 608)
+        configs.hm_size = (152, 152)
+        configs.down_ratio = 4
+        configs.max_objects = 50
+
+        configs.imagenet_pretrained = True
+        configs.head_conv = 64
+        configs.num_classes = 3
+        configs.num_center_offset = 2
+        configs.num_z = 1
+        configs.num_dim = 3
+        configs.num_direction = 2  # sin, cos
+        configs.peak_thresh =0.2
+        configs.K =50
+        configs.min_iou =0.5
+
+        configs.heads = {
+        'hm_cen': configs.num_classes,
+        'cen_offset': configs.num_center_offset,
+        'direction': configs.num_direction,
+        'z_coor': configs.num_z,
+        'dim': configs.num_dim
+        }
+        configs.num_input_features = 4
 
         #######
         ####### ID_S3_EX1-3 END #######     
@@ -118,6 +164,13 @@ def create_model(configs):
         ####### ID_S3_EX1-4 START #######     
         #######
         print("student task ID_S3_EX1-4")
+        #model = fpn_resnet(configs) 
+        
+        arch_parts = configs.arch.split('_')
+        num_layers = int(arch_parts[-1])
+        
+        model = fpn_resnet.get_pose_net( num_layers=num_layers,heads=configs.heads, head_conv=configs.head_conv,
+                                        imagenet_pretrained=configs.imagenet_pretrained)
 
         #######
         ####### ID_S3_EX1-4 END #######     
@@ -180,15 +233,16 @@ def detect_objects(input_bev_maps, model, configs):
     objects = [] 
 
     ## step 1 : check whether there are any detections
+    
 
         ## step 2 : loop over all detections
-        
             ## step 3 : perform the conversion using the limits for x, y and z set in the configs structure
-        
+
+            
             ## step 4 : append the current object to the 'objects' array
+                                       
         
     #######
     ####### ID_S3_EX2 START #######   
-    
     return objects    
 
